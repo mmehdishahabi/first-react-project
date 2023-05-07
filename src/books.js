be92b68fa67db3cb61426a30c6dd964940ed6e66
Luffy <m.mehdishahabi@gmail.com>
2023-05-07 11:09:54 +0430

    first commit

---
+++ src/books.js
@@ -0,0 +1,23 @@
+import img1 from "./images/happyPlace.jpg";
+import img2 from "./images/cleanCode.jpg";
+import img3 from "./images/luffy.jpg";
+export const books = [
+  {
+    author: "Jordan Moore",
+    title: "Interesting Facts For Curious Minds",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "Uncle bobb",
+    title: "Clean Code",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "ichiro oda",
+    title: "One Piece",
+    img: img3,
+    id: 3,
+  },
+];
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { books } from "./books";
+import Book from "./Book";
+function BookList() {
+  return (
+    <>
+      <h1>Best Seller Books</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} number={index} />;
+        })}
+      </section>
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BookList />);
